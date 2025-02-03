057ebe1d4ea8922f9de195c87ad22ac4ee7e36c1
Jose Garcia <j.edgji@gmail.com>
2025-02-03 15:59:39 -0800

    add react-router-hono-server to load the same `workers/app.ts` entrypoint in dev and apply hono context middleware

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./workers/app.ts",
-        }
-      : undefined,
-  },
+export default defineConfig(() => ({
   plugins: [
-    cloudflareDevProxy({
-      getLoadContext({ context }) {
-        return { cloudflare: context.cloudflare };
-      },
+    cloudflareDevProxy(),
+    reactRouterHonoServer({
+      runtime: "cloudflare",
+      serverEntryPoint: "workers/app.ts",
     }),
     tailwindcss(),
     reactRouter(),
